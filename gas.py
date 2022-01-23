7e74073a7642cc8e9f0b89fb8d67fde13d462111
Shady Attia <47541895+AttiaFX@users.noreply.github.com>
2022-01-23 17:22:26 +0000

    Update gas.py

--- gas.py
+++ gas.py
@@ -1,2 +1,11 @@
+import requests
+import os
+
 if __name__ == "__main__":
-    print("test")
+    # api_token = "7D8W6IGNBH967ECR2IN1K5VV2Q1IP9R5U6"
+    api_token = os.environ["ETHERSCAN_API_TOKEN"]
+    url = f"https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey={api_token}"
+    response = requests.get(url)
+    data = response.json()
+    fee = data["result"]["suggestBaseFee"]
+    print(fee)
